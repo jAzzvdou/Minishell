f9f2121072d2573becd0137c31959dcff45516e5
jAzzvdou <joaoazevedo592@gmail.com>
2024-05-31 14:06:34 -0300

    Up: Organization.

--- main.c
+++ main.c
@@ -1,55 +1,5 @@
 #include "minishell.h"
 
-void	free_matrix(char ***matrix)
-{
-	int	i;
-
-	//| Caso não tenha matrix, só retorna.
-	if (!*matrix)
-		return ;
-	i = -1;
-	while ((*matrix)[++i])
-	{
-		free((*matrix)[i]); //| Free direto no endereço.
-		(*matrix)[i] = NULL;//| Setando para NULL para não ter double free.
-	}
-	free(*matrix); //| Libera a matrix.
-	*matrix = NULL;
-}
-
-char	**static_env(char **new_envp, int clear)
-{
-	//| Static para poder acessar em outras partes do programa.
-	static char	**env;
-
-	//| Caso tenha um novo ENV, limpa o antigo.
-	if (new_envp && env)
-		free_matrix(&env);
-	//| Atualiza o antigo ENV para o novo.
-	if (new_envp)
-		env = new_envp;
-	//| Caso precise limpar o ENV.
-	if (env && clear)
-		free_matrix(&env);
-	return (env);
-}
-
-void	start_env(char **envp)
-{
-	int	i;
-	char	**new_envp;
-
-	i = 0;
-	while (envp[i])
-		i++;
-	new_env = malloc(sizeof(char *) * (i + 1));
-	i = -1;
-	while (envp[++i])
-		new_env[i] = ft_strdup(envp[i]);
-	new_env[i] = NULL;
-	static_env(new_envp, NOFREE);
-}
-
 char	*user_input(void)
 {
 	char	*input;
