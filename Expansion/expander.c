2ef77cfecae6727ae8a3f7c7392baa2adfe1d30d
jAzzvdou <joaoazevedo592@gmail.com>
2024-07-16 17:08:21 -0300

    Up: Expander.

--- Expansion/expander.c
+++ Expansion/expander.c
@@ -2,13 +2,32 @@
 
 //| AQUI VAMOS EXPANDIR O WILDCARD E AS VARIÁVEIS DE AMBIENTE.
 
+t_tokens	*wildcard(t_tokens *tokens)
+{
+	t_tokens	*tmp;
+	t_tokens	*expanded;
+
+	tmp = tokens;
+	while (tmp)
+	{
+		if (tmp->first->type == CMD
+			&& ft_strchr(tmp->first->cmd, '*'))
+		{
+			//| Pegar a lista depois de *.
+			//| tokens -> tokens expandidos.
+		}
+		tmp = tmp->next;
+	}
+	return (expanded);
+}
+
 t_tokens	*expander(t_tokens *tokens)
 {
 	t_tokens	*expanded;
 	t_tokens	*tmp;
 
 	tmp = wildcard(tokens); //| Expandir *.
-	expanded = expansion(tmp); //| Expandir VAR.
+	expanded = expansion_var(tmp); //| Expandir VAR.
 	free(tmp);
 	tmp = NULL;
 	return (expanded);
